8e40baac8f1488c4056bcf86c2cd6896a01b56e2
Esro_Dis <hemoo.alharbi2021@gmail.com>
2026-05-24 02:44:26 +0300

    new

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get("/review", async (req, res) => {
         const avatarImg = await loadImage(avatarResp.data);
 
         const avatarCenterX = 180; // يمين شوي
-        const avatarCenterY = 180; // نزلناه شوي
+        const avatarCenterY = 250; // نزلناه شوي
         const avatarRadius = 60;
 
         ctx.save();
