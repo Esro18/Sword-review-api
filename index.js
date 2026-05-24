844be1c247f7e1de16beef7533c4db7ca9b6b06b
Esro_Dis <hemoo.alharbi2021@gmail.com>
2026-05-24 03:39:18 +0300

    new

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get("/review", async (req, res) => {
         ctx.restore();
 
         // 🟦 الاسم داخل المربع الصغير بجانب الدائرة (إحداثيات مستقلة)
-        const nameX = 360; // يمين أو يسار
+        const nameX = 400; // يمين أو يسار
         const nameY = 225; // فوق أو تحت
 
         ctx.fillStyle = "#ffffff";
